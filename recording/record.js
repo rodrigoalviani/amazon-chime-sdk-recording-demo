ffb8ad798f548705f5b30b1c6d77036bdeea54bf
Rodrigo Alviani <rodrigoalviani@gmail.com>
2020-05-20 14:16:54 -0300

    Fix codec

--- recording/record.js
+++ recording/record.js
@@ -17,7 +17,7 @@ console.log(`[recording process] BROWSER_SCREEN_WIDTH: ${BROWSER_SCREEN_WIDTH}, 
 const VIDEO_BITRATE = 3000;
 const VIDEO_FRAMERATE = 30;
 const VIDEO_GOP = VIDEO_FRAMERATE * 2;
-const AUDIO_BITRATE = '128k';
+const AUDIO_BITRATE = '160k';
 const AUDIO_SAMPLERATE = 44100;
 const AUDIO_CHANNELS = 2
 const DISPLAY = process.env.DISPLAY;
@@ -26,11 +26,32 @@ const transcodeStreamToOutput = spawn('ffmpeg',[
     '-hide_banner',
     '-loglevel', 'error',
     // disable interaction via stdin
-    '-vn',
     '-nostdin',
+    // screen image size
+    '-s', `${BROWSER_SCREEN_WIDTH}x${BROWSER_SCREEN_HEIGHT}`,
+    // video frame rate
+    '-r', `${VIDEO_FRAMERATE}`,
+    // hides the mouse cursor from the resulting video
+    '-draw_mouse', '0',
+    // grab the x11 display as video input
+    '-f', 'x11grab',
+        '-i', `${DISPLAY}`,
+    // grab pulse as audio input
     '-f', 'pulse',
         '-ac', '2',
         '-i', 'default',
+    // codec video with libx264
+    '-c:v', 'libx264',
+        '-pix_fmt', 'yuv420p',
+        '-profile:v', 'main',
+        '-preset', 'veryfast',
+        '-x264opts', 'nal-hrd=cbr:no-scenecut',
+        '-minrate', `${VIDEO_BITRATE}`,
+        '-maxrate', `${VIDEO_BITRATE}`,
+        '-g', `${VIDEO_GOP}`,
+    // apply a fixed delay to the audio stream in order to synchronize it with the video stream
+    '-filter_complex', 'adelay=delays=1000|1000',
+    // codec audio with aac
     '-c:a', 'aac',
         '-b:a', `${AUDIO_BITRATE}`,
         '-ac', `${AUDIO_CHANNELS}`,
